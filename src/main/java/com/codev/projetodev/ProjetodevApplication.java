bf1d9a982201e8b20720b972a9cfce7faadb648a
Fabien GOUBIN <goubin.fabien@gmail.com>
2021-10-22 10:29:41 +0200

    Initial commit

---
+++ src/main/java/com/codev/projetodev/ProjetodevApplication.java
@@ -0,0 +1,13 @@
+package com.codev.projetodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetodevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codev/projetodev/ProjetodevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codev.projetodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetodevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
