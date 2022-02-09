f1d2d806a018e684eb8429a118c32cc9ed52f416
Fabien <goubin.fabien@gmail.com>
2022-02-09 11:17:15 +0100

    Done

--- src/main/java/com/codev/projetodev/Translate/SwitchLang.java
+++ src/main/java/com/codev/projetodev/Translate/SwitchLang.java
@@ -4,7 +4,7 @@ public enum SwitchLang {
 
     Energy("Energie"), Industrial_Processes("Processus industriels"), Land_Use_Change_and_Forestry("Changement d’affectation des terres et foresterie"),
     Bunker_Fuels("Soutes de Fiouls"), Electricity_Heat("Electricite et Chaleur"), Manufacturing_Construction("Fabrication et construction"),
-    Transportation("Transport"), Building("Construction"),Other_Fuel_Combustion("Autre combustible");
+    Transportation("Transport"), Building("Construction"),Other_Fuel_Combustion("Autre combustible"), Total_including_LUCF("Total incluant LUCF");
 
     private String translation;
 
