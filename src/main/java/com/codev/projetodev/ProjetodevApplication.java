fdd6af70fe7ae0facad3f81c6ffd626ba396a500
Fabien <goubin.fabien@gmail.com>
2022-02-10 10:34:46 +0100

    add

--- src/main/java/com/codev/projetodev/ProjetodevApplication.java
+++ src/main/java/com/codev/projetodev/ProjetodevApplication.java
@@ -3,14 +3,20 @@ package com.codev.projetodev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class ProjetodevApplication {
+public class ProjetodevApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjetodevApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ProjetodevApplication.class);
+    }
 }
